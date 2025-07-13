f4e2f9d1614150311e12c3c0f7b178dac37ae87c
luna-tender <luna_tender2015@proton.me>
2025-07-13 06:54:06 -0400

    indent line

--- init.lua
+++ init.lua
@@ -257,7 +257,13 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   { 'NMAC427/guess-indent.nvim' }, -- Detect tabstop and shiftwidth automatically
-
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
